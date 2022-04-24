8ef9121980f95852bf305820b3cf77716ce656f5
WD Snoeijer <88512452+dsnoeijer@users.noreply.github.com>
2022-04-25 00:25:37 +0200

    added upload form for new questions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var routes = require("./routes");
 var app = express();
+var cloudinary = require('cloudinary');
 
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
@@ -9,6 +10,13 @@ app.set("view engine", "ejs");
 
 app.use(routes);
 
+cloudinary.config({
+    cloud_name: 'db3rmcxpe',
+    api_key: '233225827957795',
+    api_secret: 'bi9yeGJsctS8x7NBSjA7ZVfecyo'
+});
+
 app.listen(app.get("port"), () => {
     console.log("Server started on port " + app.get("port"));
 }); 0
+
