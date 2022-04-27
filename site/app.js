9ae7b748120e99579398ff7fe958ed3c83a07af0
WD Snoeijer <88512452+dsnoeijer@users.noreply.github.com>
2022-04-28 00:05:31 +0200

    changed localhost to 127.0.0.1

--- site/app.js
+++ site/app.js
@@ -64,30 +64,30 @@ app.get("", (req, res) => {
 // Creating one
 app.post('/uploads', async (req, res) => {
     try {
-        if (!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-            let avatar = req.files.imageUpload;
+        // if (!req.files) {
+        //     res.send({
+        //         status: false,
+        //         message: 'No file uploaded'
+        //     });
+        // } else {
+        //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+        let avatar = req.files.imageUpload;
 
-            //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            avatar.mv('./uploads/' + avatar.name);
+        //Use the mv() method to place the file in upload directory (i.e. "uploads")
+        avatar.mv('./uploads/' + avatar.name);
 
 
-            //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: avatar.name,
-                    mimetype: avatar.mimetype,
-                    size: avatar.size
-                }
-            });
-        }
+        //send response
+        res.send({
+            status: true,
+            message: 'File is uploaded',
+            data: {
+                name: avatar.name,
+                mimetype: avatar.mimetype,
+                size: avatar.size
+            }
+        });
+        // }
     } catch (err) {
         res.status(500).send(err);
     }
